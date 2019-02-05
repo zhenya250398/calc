d75a2ae21a1a1d566a806a19d8f3e35e96351882
zhenya250398 <zhenya250398@gmail.com>
2019-02-05 12:54:09 +0500

    написал тесты

--- src/test/java/com/zhenya250398/calc/CalcTest.java
+++ src/test/java/com/zhenya250398/calc/CalcTest.java
@@ -1,7 +1,56 @@
 package com.zhenya250398.calc;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.apache.commons.lang3.RandomUtils.nextDouble;
+import static org.apache.commons.lang3.RandomUtils.nextInt;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalcTest {
+    public static final int LIMIT = 5;
 
+    private static Stream<Arguments> numGen() {
+        return Stream.generate(() -> Arguments.of(nextDouble(),nextDouble())).limit(LIMIT);
+    }
+
+    private static double operation(double num1,String op,double num2){
+        if("+".equals(op)){
+            return num1+num2;
+        } else if("-".equals(op)){
+            return num1-num2;
+        } else if("*".equals(op)){
+            return num1*num2;
+        } else if("/".equals(op)){
+            return num1/num2;
+        } else return  0;
+    }
+
+    @ParameterizedTest
+    @MethodSource("numGen")
+    void testCalcOne(double num1,double num2){
+        Assertions.assertEquals( operation(num1,"+",num2) , Calc.opManager(num1,"+", num2));
+    }
+
+    @ParameterizedTest
+    @MethodSource("numGen")
+    void testCalcTwo(double num1,double num2){
+        Assertions.assertEquals( operation(num1,"-",num2) , Calc.opManager(num1,"-", num2));
+    }
+
+    @ParameterizedTest
+    @MethodSource("numGen")
+    void testCalcThree(double num1,double num2){
+        Assertions.assertEquals( operation(num1,"*",num2) , Calc.opManager(num1,"*", num2));
+    }
+
+    @ParameterizedTest
+    @MethodSource("numGen")
+    void testCalcFour(double num1,double num2){
+        Assertions.assertEquals( operation(num1,"/",num2) , Calc.opManager(num1,"/", num2));
+    }
 }
--- src/test/java/com/zhenya250398/calc/DivTest.java
+++ src/test/java/com/zhenya250398/calc/DivTest.java
@@ -1,7 +1,26 @@
 package com.zhenya250398.calc;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.apache.commons.lang3.RandomUtils.nextDouble;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DivTest {
+    public static final int LIMIT = 10;
 
+    private static Stream<Arguments> numGen() {
+        return Stream.generate(() -> Arguments.of(nextDouble(),nextDouble())).limit(LIMIT);
+    }
+
+    @ParameterizedTest
+    @MethodSource("numGen")
+    void testDivOne(double num1,double num2){
+
+        Assertions.assertEquals( num1/num2 , Div.div(num1, num2));
+    }
 }
