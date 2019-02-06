91e31e5eb9fd70324cb11369563edb7d6e4c14dd
zhenya250398 <zhenya250398@gmail.com>
2019-02-06 15:10:25 +0500

    тестирование деления на ноль

--- src/main/java/com/zhenya250398/calc/Div.java
+++ src/main/java/com/zhenya250398/calc/Div.java
@@ -3,6 +3,9 @@ package com.zhenya250398.calc;
 public class Div implements MathOperation{
     @Override
     public double perform(double num1, double num2){
+        if(num2==0){
+            throw new ArithmeticException ("zero division");
+        }
         double res=num1/num2;
         return  res;
     }
--- src/test/java/com/zhenya250398/calc/CalcTest.java
+++ src/test/java/com/zhenya250398/calc/CalcTest.java
@@ -1,7 +1,5 @@
 package com.zhenya250398.calc;
 
-import org.junit.Before;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,14 +7,13 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 
 import java.util.stream.Stream;
 
 import static org.apache.commons.lang3.RandomUtils.nextDouble;
-import static org.apache.commons.lang3.RandomUtils.nextInt;
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 import static org.mockito.MockitoAnnotations.initMocks;
+
 @TestInstance(PER_CLASS)
 class CalcTest {
     public static final int LIMIT = 5;
@@ -30,43 +27,47 @@ class CalcTest {
     private MathOperation mul;
     @Mock
     private MathOperation div;
+
     @BeforeAll
-    public void before(){
+    public void before() {
         initMocks(this);
-        this.calc=new Calc(sum,sub,mul,div);
+        this.calc = new Calc(sum, sub, mul, div);
     }
 
 
     private static Stream<Arguments> numGen() {
-        return Stream.generate(() -> Arguments.of(nextDouble(),nextDouble())).limit(LIMIT);
+        return Stream.generate(() -> Arguments.of(nextDouble(), nextDouble())).limit(LIMIT);
     }
 
     @ParameterizedTest
     @MethodSource("numGen")
-    void testCalcSum(double num1,double num2){
-       calc.calculation(num1,"+",num2);
-       Mockito.verify(sum).perform(num1,num2);
-       Mockito.verifyZeroInteractions(sub,mul,div);
+    void testCalcSum(double num1, double num2) {
+        calc.calculation(num1, "+", num2);
+        Mockito.verify(sum).perform(num1, num2);
+        Mockito.verifyZeroInteractions(sub, mul, div);
     }
 
     @ParameterizedTest
     @MethodSource("numGen")
-    void testCalcSub(double num1,double num2){
-        calc.calculation(num1,"-",num2);
-        Mockito.verify(sub).perform(num1,num2);
-        Mockito.verifyZeroInteractions(sum,mul,div);    }
+    void testCalcSub(double num1, double num2) {
+        calc.calculation(num1, "-", num2);
+        Mockito.verify(sub).perform(num1, num2);
+        Mockito.verifyZeroInteractions(sum, mul, div);
+    }
 
     @ParameterizedTest
     @MethodSource("numGen")
-    void testCalcMul(double num1,double num2){
-        calc.calculation(num1,"*",num2);
-        Mockito.verify(mul).perform(num1,num2);
-        Mockito.verifyZeroInteractions(sub,sum,div);    }
+    void testCalcMul(double num1, double num2) {
+        calc.calculation(num1, "*", num2);
+        Mockito.verify(mul).perform(num1, num2);
+        Mockito.verifyZeroInteractions(sub, sum, div);
+    }
 
     @ParameterizedTest
     @MethodSource("numGen")
-    void testCalcDiv(double num1,double num2){
-        calc.calculation(num1,"/",num2);
-        Mockito.verify(div).perform(num1,num2);
-        Mockito.verifyZeroInteractions(sub,mul,sum);    }
+    void testCalcDiv(double num1, double num2) {
+        calc.calculation(num1, "/", num2);
+        Mockito.verify(div).perform(num1, num2);
+        Mockito.verifyZeroInteractions(sub, mul, sum);
+    }
 }
