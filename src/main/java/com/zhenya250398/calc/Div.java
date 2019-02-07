8c0c773b48c155d4a554951f9191dc70cf1cef73
zhenya250398 <zhenya250398@gmail.com>
2019-02-07 14:38:25 +0500

    тестирование деления на ноль

--- src/main/java/com/zhenya250398/calc/Div.java
+++ src/main/java/com/zhenya250398/calc/Div.java
@@ -3,7 +3,7 @@ package com.zhenya250398.calc;
 public class Div implements MathOperation{
     @Override
     public double perform(double num1, double num2){
-        if(num2==0){
+        if(num2==0.0d){
             throw new ArithmeticException ("zero division");
         }
         double res=num1/num2;
